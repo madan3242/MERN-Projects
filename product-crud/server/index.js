398685f185fbbb0e089f7db6f9aeb7d1b062e7b9
Madan <98645368+madan3242@users.noreply.github.com>
2023-01-10 17:27:00 +0530

    Delete product-crud directory

--- product-crud/server/index.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-import { connectDb } from "./config/db.js";
-
-
-app.listen(process.env.PORT, process.env.HOSTNAME, () => {
-    connectDb();
-    console.log(`SERVER RUNNING http://${process.env.HOSTNAME}:${process.env.PORT}`);
-})
